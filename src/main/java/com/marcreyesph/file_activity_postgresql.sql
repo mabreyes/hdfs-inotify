e038e78bebb5746884a7b257bec0c334d2328442
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2019-07-28 22:38:43 +0800

    Declare file sql code in psql

--- src/main/java/com/marcreyesph/file_activity_postgresql.sql
+++ src/main/java/com/marcreyesph/file_activity_postgresql.sql
@@ -1,6 +1,14 @@
 DROP TABLE IF EXISTS file_activity;
 
 CREATE TABLE IF NOT EXISTS file_activity (
-    file_change_id serial PRIMARY KEY;
-
+    fileChangeId serial PRIMARY KEY,
+    transactionId INTEGER NOT NULL,
+    eventType VARCHAR(100) NOT NULL,
+    path VARCHAR(1000) NOT NULL,
+    ownerName VARCHAR(500) NULL,
+    cTime TIMESTAMP NULL,
+    timeStamp TIMESTAMP NULL,
+    fileSize INTEGER NULL,
+    dstPath VARCHAR(1000) NULL,
+    srcPath VARCHAR(1000) NULL
 );
