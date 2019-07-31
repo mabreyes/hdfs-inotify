d89d0a32f2a3492f4e84e2bf1df2536c9d04e547
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2019-07-31 13:28:58 +0800

    Make column null

--- src/main/java/com/marcreyesph/file_activity_postgresql.sql
+++ src/main/java/com/marcreyesph/file_activity_postgresql.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS file_activity;
+DROP TABLE IF EXISTS file_activity_2;
 
-CREATE TABLE IF NOT EXISTS file_activity (
+CREATE TABLE IF NOT EXISTS file_activity_2 (
     fileChangeId SERIAL PRIMARY KEY,
-    transactionId INTEGER NOT NULL,
-    eventType VARCHAR(100) NOT NULL,
+    transactionId VARCHAR(1000) NOT NULL,
+    eventType VARCHAR(1000) NOT NULL,
     path VARCHAR(1000) NOT NULL,
-    ownerName VARCHAR(500) NULL,
-    cTime TIMESTAMP NULL,
-    timeStamp TIMESTAMP NULL,
+    ownerName VARCHAR(1000) NULL,
+    cTime VARCHAR(1000) NULL,
+    timeStamp VARCHAR(1000) NULL,
     fileSize INTEGER NULL,
     dstPath VARCHAR(1000) NULL,
     srcPath VARCHAR(1000) NULL
